f3b2c61af38c0a74ee13abc21c44f98470102194
Grace Grimwood <ggrimwoo@redhat.com>
2023-11-27 16:43:40 +1300

    parameterise remaining bootstrap version strings in setup script

--- bootstrap_setup.sh
+++ bootstrap_setup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-export BOOTSTRAP_VERSION="v5.3.2"
+export BOOTSTRAP_VERSION="5.3.2"
 
 # download the full Bootstrap 5 sources (including JS)
-echo "Downloading Bootstrap $BOOTSTRAP_VERSION sources from GitHub..."
-wget "https://github.com/twbs/bootstrap/archive/$BOOTSTRAP_VERSION.zip"
+echo "Downloading Bootstrap v$BOOTSTRAP_VERSION sources from GitHub..."
+wget "https://github.com/twbs/bootstrap/archive/v$BOOTSTRAP_VERSION.zip"
 
 # unpack the sources into the _sass/ directory
 echo "Unpacking sources..."
-unzip "$BOOTSTRAP_VERSION.zip" -d ./_sass
+unzip "v$BOOTSTRAP_VERSION.zip" -d ./_sass
 
 # make a directory to put the Bootstrap 5 Sass sources into
 mkdir ./_sass/bootstrap
 
 # move the Sass sources to your new directory
-mv ./_sass/bootstrap-5.3.2/scss ./_sass/bootstrap/scss
+mv "./_sass/bootstrap-$BOOTSTRAP_VERSION/scss" ./_sass/bootstrap/scss
 
 # delete all the other Bootstrap sources (i.e. except the Sass ones, which we moved)
 echo "Cleaning up..."
-rm -r ./_sass/bootstrap-5.3.2
+rm -r "./_sass/bootstrap-$BOOTSTRAP_VERSION"
 
 # delete the downloaded zip
-rm v5.3.2.zip
+rm "v$BOOTSTRAP_VERSION.zip"
 
 echo "Done!"
