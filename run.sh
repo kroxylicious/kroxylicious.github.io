3574fa77ad7dfcbdac2f152e97607e20c4fac28e
Robert Young <robeyoun@redhat.com>
2025-07-01 16:45:01 +1200

    Add a Dockerfile and script to run a development jekyll server
    Co-authored-by: Keith Wall<kwall@apache.org>
    Signed-off-by: Robert Young <robeyoun@redhat.com>

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+OS=$(uname)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}
+CONTAINER_ENGINE=${CONTAINER_ENGINE:-podman}
+
+RUN_ARGS=()
+if [ "$OS" = 'Darwin' ]; then
+   RUN_ARGS+=(--env JEKYLL_SERVE_BIND=0.0.0.0 --publish 4000:4000)
+else
+   RUN_ARGS+=(--net host)
+fi
+RUN_ARGS+=(--rm -it kroxylicious-website)
+
+${CONTAINER_ENGINE} build . -t kroxylicious-website 
+
+${CONTAINER_ENGINE} run "${RUN_ARGS[@]}"
